163fc606c9666af8fe908a3e7793e3630369af3e
Marcus <suwacc@scrombled.dev>
2023-07-08 13:47:49 +1000

    working cmp, autocompletion and lsp. very messy

--- init.lua
+++ init.lua
@@ -27,40 +27,76 @@ local plugins = {
         dependencies = {
             "nvim-tree/nvim-web-devicons",
         },
-	keys = {
-	    {"<leader>e", "<cmd>NvimTreeFocus<cr>", desc = "Focus NvimTree"},
-	    {"<C-n>", "<cmd>NvimTreeToggle<cr>", desc = "Toggle NvimTree"},
-	},
+        keys = {
+            { "<leader>e", "<cmd>NvimTreeFocus<cr>",  desc = "Focus NvimTree" },
+            { "<C-n>",     "<cmd>NvimTreeToggle<cr>", desc = "Toggle NvimTree" },
+        },
     },
     {
-	"neovim/nvim-lspconfig",
-	version = "*",
+        "neovim/nvim-lspconfig",
+        version = "*",
     },
     {
-	"nvim-lualine/lualine.nvim",
-	version = "*",
-	dependencies = {
-	    "nvim-tree/nvim-web-devicons",
+        "nvim-lualine/lualine.nvim",
+        version = "*",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
         }
     },
     {
-	"folke/tokyonight.nvim",
-	version = "*",
-	lazy = false,
-	priority = 1000,
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = false,
+        priority = 1000,
     },
     {
-	"nvim-treesitter/nvim-treesitter",
-	version = "*",
-	build = ":TSUpdate",
+        "nvim-treesitter/nvim-treesitter",
+        version = "*",
+        build = ":TSUpdate",
     },
     {
-	"windwp/nvim-autopairs",
-	version = "*",
+        "windwp/nvim-autopairs",
+        version = "*",
     },
     {
-	"lewis6991/gitsigns.nvim",
-	version = "*",
+        "lewis6991/gitsigns.nvim",
+        version = "*",
+    },
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        version = "*"
+    },
+    {
+        "hrsh7th/cmp-nvim-lsp",
+        version = "*"
+    },
+    {
+        "hrsh7th/cmp-buffer",
+        version = "*"
+    },
+    {
+        "hrsh7th/cmp-path",
+        version = "*"
+    },
+    {
+        "hrsh7th/cmp-cmdline",
+        version = "*"
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        version = "*"
+    },
+    {
+        "L3MON4D3/LuaSnip",
+        version = "*",
+        dependencies = { "rafamadriz/friendly-snippets" }
+    },
+    {
+        "saadparwaiz1/cmp_luasnip",
+        version = "*"
+    },
+    {
+        "rafamadriz/friendly-snippets",
     },
 }
 
@@ -73,28 +109,125 @@ require("mason").setup()
 require("nvim-tree").setup()
 require("lualine").setup({
     options = {
-        theme = 'tokyonight'
+        theme = 'catppuccin'
     }
 })
-require("tokyonight").setup({
-    style = "storm",
-    light_style = "day",
-    terminal_colors = true
+require("catppuccin").setup({
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        telescope = true,
+        treesitter = true,
+    }
 })
 require("nvim-treesitter.configs").setup({
     ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "javascript" },
     highlight = {
-	enable = true,
+        enable = true,
+        use_languagetree = true,
     },
     indent = {
-	enable = true,
+        enable = true,
     },
 })
 require("nvim-autopairs").setup()
 require("gitsigns").setup()
 
-vim.cmd([[colorscheme tokyonight]])
+local null_ls = require("null-ls")
+local sources = {
+    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.formatting.eslint_d,
+    null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.completion.luasnip
+}
+null_ls.setup({ sources = sources })
+
+local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local luasnip = require("luasnip")
+local cmp = require("cmp")
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered()
+    },
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'buffer' }
+    },
+    mapping = {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+                -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+                -- they way you will only jump inside the snippet region
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+    },
+})
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'buffer' }
+    })
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    })
+})
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+local lspconfig = require('lspconfig')
+
+-- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
+for _, lsp in ipairs(servers) do
+    lspconfig[lsp].setup {
+        -- on_attach = my_custom_on_attach,
+        capabilities = capabilities,
+    }
+end
+require("luasnip.loaders.from_vscode").lazy_load()
+
+vim.cmd([[colorscheme catppuccin]])
 vim.cmd([[set number]])
+
+vim.opt.completeopt = 'menuone,noselect'
+vim.lsp.set_log_level('trace')
 
 -- Telescope Keymapping
 local builtin = require("telescope.builtin")
